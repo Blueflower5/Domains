6f85088b9be41c24e2a976b103d277423d86c6ac
moham <mohammad9776@gmail.com>
2025-05-05 14:35:15 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <>
+      <div>Domains</div>
+      <div>
+        <button>+ Add Domain</button>
+        <span>sort</span>
+        <span>search</span>
+      </div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+      <div>Add Domain</div>
+      <input type="text"></input>
+      <div>
+        <button>cancel</button>
+        <button>Add</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
